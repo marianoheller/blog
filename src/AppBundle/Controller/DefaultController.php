3738abb0645ce9f773789f232905bc7ad607aba8
Mariano Heller <marianoheller91@gmail.com>
2016-04-20 17:09:45 -0300

    Wraping up. Emprolijé comentarios.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -26,10 +26,6 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        /*return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);*/
         return $this->redirectToRoute('blog');
     }
 
